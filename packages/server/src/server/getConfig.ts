71cb0ed336100254de09d2d94957d3d42a685eb5
Dean Slama <deanslamajr@gmail.com>
2020-09-25 23:32:46 -0600

    server: finished wiring up config loader

--- packages/server/src/server/getConfig.ts
+++ packages/server/src/server/getConfig.ts
@@ -1,30 +1,52 @@
 import path from 'path';
 import findUp from 'find-up';
+import editJsonFile from 'edit-json-file';
 
 const CONFIG_FILE_NAME = 'grogqli.json';
 
-interface GrogqliConfig {
+export interface GrogqliConfig {
   recordingsSaveDirectory: string;
   recordingsFilename: string;
 }
 
 const DEFAULT_CONFIG: GrogqliConfig = {
-  recordingsSaveDirectory: '/home/dhan/codes/grogqli',
+  recordingsSaveDirectory: process.cwd(),
   recordingsFilename: 'groql_rec'
 };
-export const getConfig = async (): Promise<GrogqliConfig> => {
-  console.log('process.cwd()', process.cwd());
-  // TODO
+
+type GetGrogqliConfigValue = (configKey: keyof GrogqliConfig) => string;
+
+export const getConfig = async (): Promise<GetGrogqliConfigValue> => {
   // recursively try to open a file with filename === CONFIG_FILE_NAME
   // if file doesn't exist, try again in parent directory
   // if file isn't found at root directory, return DEFAULT_CONFIG
-  const configLocation = await findUp(CONFIG_FILE_NAME);
+  const configLocation = await findUp(async directory => {
+    const hasConfig = await findUp.exists(path.join(directory, CONFIG_FILE_NAME));
+    return hasConfig
+      ? directory
+      : undefined;
+  }, {type: 'directory'});
 
-  
-  console.log('configLocation', configLocation)
+  let configFromFile: editJsonFile.JsonEditor;
 
-  // TODO
-  // if a config doesn't have a value defined for a particular lookup
-  // use the value found in DEFAULT_CONFIG
-  return DEFAULT_CONFIG;
+  if (configLocation) {
+    const configFileAbsolutePath = path.join(configLocation, CONFIG_FILE_NAME);
+    configFromFile = editJsonFile(configFileAbsolutePath);
+  }
+
+  const getGrogqliConfigValue: GetGrogqliConfigValue = (configKey) => {
+    let configValue: any;
+    if (configFromFile) {
+      configValue = configFromFile.get(configKey);
+      if (configKey === 'recordingsSaveDirectory' && configValue !== undefined) {
+        configValue = `${configLocation}/${configValue}`
+      }
+    }
+    if (configValue === undefined) {
+      configValue = DEFAULT_CONFIG[configKey];
+    }
+    return configValue;
+  };
+
+  return getGrogqliConfigValue;
 }
--- packages/server/src/server/recorder.ts
+++ packages/server/src/server/recorder.ts
@@ -1,23 +1,31 @@
 import gql from 'graphql-tag';
+import fs from 'fs';
 import { OperationDefinitionNode, FieldNode} from 'graphql';
 import editJsonFile from 'edit-json-file';
 
 import { getConfig } from './getConfig';
+
+const getRecordingFile = async (): Promise<editJsonFile.JsonEditor> => {
+  const config = await getConfig();
+  const recordingsSaveDirectory = config('recordingsSaveDirectory');
+  if (!fs.existsSync(recordingsSaveDirectory)){
+    fs.mkdirSync(recordingsSaveDirectory);
+  }
+
+  return editJsonFile(`${recordingsSaveDirectory}/${config('recordingsFilename')}.json`);
+}
 
 export const recordQuery = async (req, res) => {
   const query = gql`
     ${req.body.query}
   `;
 
-  const config = await getConfig();
-
-  const file = editJsonFile(`${config.recordingsSaveDirectory}/${config.recordingsFilename}.json`);
-
   const recording = {
     type: (query.definitions[0] as OperationDefinitionNode).operation,
     name: ((query.definitions[0] as OperationDefinitionNode).selectionSet.selections[0] as FieldNode).name.value,
   };
 
+  const file = await getRecordingFile();
   file.set(req.params.id, recording);
 
   file.save();
@@ -26,17 +34,16 @@ export const recordQuery = async (req, res) => {
 };
 
 export const updateRecording = async (req, res) => {
-  const config = await getConfig();
-  const file = editJsonFile(`${config.recordingsSaveDirectory}/${config.recordingsFilename}.json`);
-
+  const file = await getRecordingFile();
   const existingRecording = file.get(req.params.id);
+
   const updatedRecording = {
     ...existingRecording,
     response: req.body,
   };
 
   file.set(req.params.id, updatedRecording);
-
   file.save();
+
   res.sendStatus(200);
 };
