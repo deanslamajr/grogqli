898ade135470952ac77827ddecab6a3d9fcfceb3
Dean Slama <deanslamajr@gmail.com>
2020-10-10 17:55:17 -0600

    server: got subscriptions on on app working

--- packages/server/src/client/App/Transactions/index.tsx
+++ packages/server/src/client/App/Transactions/index.tsx
@@ -1,22 +1,61 @@
 import React, { useEffect, useState} from 'react';
 import { useQuery, useSubscription, ApolloError } from '@apollo/client';
-import {GetRecordings, OnRecordingSaved} from '@grogqli/schema';
+import {GetRecordings, OnRecordingSaved, Recording} from '@grogqli/schema';
 
 import './index.css';
 
 const TransactionsWithData: React.FC = () => {
-  const {data, loading, error} = useQuery(
+  const {data, loading, error, subscribeToMore} = useQuery(
     GetRecordings.GetRecordingsDocument,
-    // { variables: { postID: params.postID } }
+    // { variables: {  } }
   );
-  console.log('data', data)
+
   if (error) {
     console.error(error);
   }
+
   return <Transactions
     recordings={data?.recordings || []}
     loading={loading}
     error={error}
+    subscribeToRecordings={() => {
+      return subscribeToMore({
+        document: OnRecordingSaved.OnRecordingSaveDocument,
+        // variables: {  },
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) {
+            return prev;
+          }
+
+          const newFeedItem = subscriptionData.data.recordingSaved;
+
+          if (newFeedItem === null) {
+            return prev;
+          }
+
+          const combineAndDedupe = () => {
+            // deep copy
+            const newSet: Recording[] = JSON.parse(JSON.stringify(prev.recordings));
+            
+            const recordedRequest = newSet.find(recording => recording.id === newFeedItem.id);
+
+            if (recordedRequest) {
+              Object.assign(recordedRequest, newFeedItem);
+              return newSet;
+            } else {
+              return [newFeedItem, ...newSet];
+            }
+          }
+
+          return Object.assign({}, prev, {
+            recordings: prev.recordings !== null
+              ? combineAndDedupe()
+              : [newFeedItem]
+            }
+          );
+        }
+      })
+    }}
   />;
 };
 
@@ -24,20 +63,31 @@ interface TransactionsProps {
   recordings: GetRecordings.Recording[];
   loading: boolean;
   error?: ApolloError;
+  subscribeToRecordings: () => void;
 }
 
-const Transactions: React.FC<TransactionsProps> = ({recordings, loading, error}) => {
+const Transactions: React.FC<TransactionsProps> = (
+  {recordings, subscribeToRecordings}
+) => {
+  React.useEffect(() => {
+    subscribeToRecordings();
+  }, []);
+
   return (<div className='Home'>
     <table>
-    <tr>
-      <th>op name</th>
-    </tr>
-      {recordings.map(transaction => (
-        <Transaction
-          key={transaction.id}
-          transaction={transaction}
-        />
-      ))}
+      <thead>
+        <tr>
+          <th>op name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {recordings.map(transaction => (
+          <Transaction
+            key={transaction.id}
+            transaction={transaction}
+          />
+        ))}
+      </tbody>
     </table>
   </div>);
 }
@@ -47,9 +97,11 @@ interface TransactionProps {
 }
 
 const Transaction: React.FC<TransactionProps> = ({transaction}) => {
-  return (<tr>
-    <td>{transaction.operationName}</td>
-  </tr>)
+  return (
+    <tr>
+      <td>{transaction.operationName}</td>
+    </tr>
+  );
 }
 
 export default TransactionsWithData;
--- packages/server/src/client/index.tsx
+++ packages/server/src/client/index.tsx
@@ -6,14 +6,36 @@ import { onError } from '@apollo/client/link/error';
 import App from './App';
 
 import {createApolloClient} from './createApolloClient';
-import { HttpLink, from } from '@apollo/client';
+import { HttpLink, from, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
 const grogqliPath = 'http://localhost:4000/grogqli';
+const grogqliWsPath = 'ws://localhost:4000/graphql';
 
 const httpLink = new HttpLink({
   uri: grogqliPath,
   credentials: 'same-origin',
 });
+
+const wsLink = new WebSocketLink({
+  uri: grogqliWsPath,
+  options: {
+    reconnect: true
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
@@ -25,7 +47,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-const link = from([errorLink, httpLink]);
+const link = from([errorLink, splitLink]);
 
 const apolloClient = createApolloClient({ link });
 
