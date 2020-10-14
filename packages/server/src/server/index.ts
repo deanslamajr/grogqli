87b85841e82fe4ed212b71549021cfb3159a62a6
Dean Slama <deanslamajr@gmail.com>
2020-10-13 19:54:16 -0600

    fixed last commit's mistake

--- packages/server/src/server/index.ts
+++ packages/server/src/server/index.ts
@@ -2,13 +2,9 @@ import express from 'express';
 import cors from 'cors';
 
 import { renderAppForAllGetPathsExceptGraphql } from './renderApp';
-import { apolloServer as _apolloServer } from './graphql';
-import { grogqliPath } from '../shared/constants';
 
-const server = express()
+export { apolloServer } from './graphql';
+export const server = express()
   .disable('x-powered-by')
   .use(cors())
   .use(renderAppForAllGetPathsExceptGraphql);
-
-_apolloServer.applyMiddleware({ app: server, path: grogqliPath });
-export const apolloServer = _apolloServer;
