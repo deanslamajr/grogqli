effa7b43fb510f565131b6a5fd5d79b99043a788
Dean Slama <deanslamajr@gmail.com>
2020-10-24 15:47:23 -0600

    server: got backend HMR working a bit better

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,28 +3,54 @@ import http from 'http';
 import { server, apolloServer } from './server';
 import { grogqliPath } from './shared/constants';
 
-let httpServer;
-
-if (module.hot) {
-  module.hot.accept('./server', () => {
-    console.log('🔁  HMR Reloading `./server`');
-    apolloServer.applyMiddleware({ app: server, path: '/grogqli' });
-    httpServer = http.createServer(server);
-    apolloServer.installSubscriptionHandlers(httpServer);
-    httpServer.listen(port, () => {
-      console.log(`> App started http://localhost:${port}`);
-    });
-  });
-  console.info('✅  Server-side HMR Enabled!');
-}
+let httpServer: http.Server | undefined;
+let httpTerminator;
 
 const port = 4000; //process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-apolloServer.applyMiddleware({ app: server, path: grogqliPath });
-httpServer = http.createServer(server);
-apolloServer.installSubscriptionHandlers(httpServer);
-httpServer.listen(port, () => {
-  console.log(`> App started http://localhost:${port}`);
-});
+const initializeServer = () => {
+  apolloServer.applyMiddleware({ app: server, path: grogqliPath });
+  httpServer = http.createServer(server);
+  apolloServer.installSubscriptionHandlers(httpServer);
+};
+
+const registerServerForSafeTermination = () => {
+  console.log('> Registering new server with http-terminator...');
+  const { createHttpTerminator } = require('http-terminator');
+  httpTerminator = createHttpTerminator({
+    server: httpServer,
+  });
+};
+
+const startServer = () => {
+  if (!httpServer) {
+    throw new Error(
+      'Attempted to start server but `httpServer` does not exist!'
+    );
+  }
+  httpServer.listen(port, () => {
+    console.log(`> Server started http://localhost:${port}`);
+  });
+};
+
+initializeServer();
+
+if (module.hot) {
+  module.hot.accept('./server', async () => {
+    console.log('🔁  HMR Reloading `./server`');
+
+    console.log('> Temporarily stopping server...');
+    await httpTerminator.terminate();
+
+    initializeServer();
+    registerServerForSafeTermination();
+    startServer();
+  });
+
+  registerServerForSafeTermination();
+  console.info('✅  Server-side HMR Enabled!');
+}
+
+startServer();
 
 export default httpServer;
