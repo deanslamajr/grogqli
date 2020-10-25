89e627431079359c388a0ffb21f8c808a811b401
Dean Slama <deanslamajr@gmail.com>
2020-10-24 20:49:53 -0600

    server: wired up resolveQueryFromRecording poc

--- packages/server/src/server/graphql/schema/resolvers/queries/__tests__/resolveQueryFromRecording.test.ts
+++ packages/server/src/server/graphql/schema/resolvers/queries/__tests__/resolveQueryFromRecording.test.ts
@@ -1,8 +1,16 @@
-import { resolver } from '../resolveQueryFromRecording';
+import {
+  resolveQueryFromRecording,
+  ResolveQueryFromRecordingParams,
+} from '../resolveQueryFromRecording';
 
 describe('resolveQueryFromRecording', () => {
   it('should execute the given query', async () => {
-    const actual = await resolver();
-    expect(actual).toBe({});
+    const query =
+      'query GetChores {\n  getChores {\n    chores {\n      id\n      summary\n      description\n      dueDate\n      version\n      __typename\n    }\n    hasAccountSession\n    __typename\n  }\n}\n';
+    const queryConfig: ResolveQueryFromRecordingParams = {
+      query,
+    };
+    const actual = await resolveQueryFromRecording(queryConfig);
+    expect(actual).toMatchSnapshot();
   });
 });
