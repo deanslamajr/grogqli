b5ec3f2a7d60e83024e829792f727d52771746b1
Dean Slama <deanslamajr@gmail.com>
2021-01-10 01:05:38 -0700

    server: made operation asset creation a bit safer, a bit slower

--- packages/server/src/server/createWorkflowAssetsFromPlan/index.ts
+++ packages/server/src/server/createWorkflowAssetsFromPlan/index.ts
@@ -20,22 +20,27 @@ export const createWorkflowAssetsFromPlan = async (
     operationRecordings: {},
   };
 
-  await Promise.all(
-    plan.opRecordingsPlans.map(async (opPlan) => {
-      // create type recording assets first so that any types that don't yet have type files
-      // can have these initialized. Operation recordings require valid typeId's to associate
-      // rootTypeRecordingId's to the appropriate root types
-      await createTypeAssetsFromPlan(opPlan);
+  // Creating new asset files can cause race conditions when executed in parallel
+  // e.g. multiple files can be created for the same type
+  // Consequently, do these jobs in sequence
+  await plan.opRecordingsPlans.reduce(
+    (sequentialAsyncTasks, opPlan) =>
+      sequentialAsyncTasks.then(async () => {
+        // create type recording assets first so that any types that don't yet have type files
+        // can have these initialized. Operation recordings require valid typeId's to associate
+        // rootTypeRecordingId's to the appropriate root types
+        await createTypeAssetsFromPlan(opPlan);
 
-      // create/update grogqli/operations/<opId>.json
-      const { opId, opRecordingId } = await createOrUpdateOpFile(opPlan);
+        // create/update grogqli/operations/<opId>.json
+        const { opId, opRecordingId } = await createOrUpdateOpFile(opPlan);
 
-      // add an entry to grogqli/workflows/<workflowId>.json#operationRecordings
-      newWorkflowFile.operationRecordings[opId] = {
-        opId,
-        opRecordingId,
-      };
-    })
+        // add an entry to grogqli/workflows/<workflowId>.json#operationRecordings
+        newWorkflowFile.operationRecordings[opId] = {
+          opId,
+          opRecordingId,
+        };
+      }),
+    Promise.resolve()
   );
 
   await createNewWorkflowAssets({
